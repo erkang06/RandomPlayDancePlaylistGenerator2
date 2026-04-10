b940229c75f185963e2d0c63aa9de354e3dbb065
Benny Guo <bennyguo06@gmail.com>
2026-04-10 19:18:02 +0100

    created project created layout added function to add countdowns to tblcountdown

---
+++ src/main/java/com/mycompany/randomplaydanceplaylistgenerator2/RandomPlayDancePlaylistGenerator2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.randomplaydanceplaylistgenerator2;
+
+/**
+ *
+ * @author benny
+ */
+public class RandomPlayDancePlaylistGenerator2 {
+
+    public static void main(String[] args) {
+        MainFrameFlatLaf f = new MainFrameFlatLaf();
+        f.setVisible(true);
+    }
+}
